1e44fc5a023d988f8e04ade70f2b1d11708ed606
joaop0102 <101288165+joaop0102@users.noreply.github.com>
2023-02-27 16:17:47 -0300

    Add files via upload

---
+++ aula2702/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 0;
+
+$b = 10;
+if($b <= 10){
+    echo "O valor da variável b é maior ou igual a 10"   
+}else{
+    echo "O  valor da variável b é menor ou igual a 10"
+}
+for($i = 0; $i <= 10; $i++){
+    echo $i;
+}
